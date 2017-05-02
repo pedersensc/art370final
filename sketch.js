300b15c67aaea7c572062f886ce35900f500ece8
Sam Pedersen <pedersensc187@gmail.com>
2017-05-02 10:15:31 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+/*function setup() {
+  createCanvas(600, 600);
+  background(255, 255, 0);
+}
+
+function draw() {
+  stroke(0);
+  strokeWeight(5);
+  line(10, 10, 10, 30);
+  line(20, 10, 20, 30);
+  line(30, 10, 30, 30);
+  point(40, 20);
+  point(50, 20);
+  point(60, 20);
+}*/
+function myFunction1() {
+    document.getElementById("myImg").style.WebkitFilter = "grayscale(100%)";
+
+    //x.style.WebkitFilter="grayscale(100%)");
+}
+
+function myFunction2() {
+  document.getElementById("myImg").style.WebkitFilter = "grayscale(0%)";
+}
